4938ad9e548bb5ce7e19830c90ffc2a1926c2a83
Salcuni <giovanni.salcuni12@gmail.com>
2019-05-27 17:03:41 +0200

    starting css

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'flats/index'
-  get 'flats/show'
+  resources :flats, only: [:show, :index]
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
