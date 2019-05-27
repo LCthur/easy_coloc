f020b029ed1ce3ec6228063a33aeeeeba9da16a5
Loic Thurre <loic.thurre@gmail.com>
2019-05-27 18:34:47 +0200

    img helper with carrierwave is working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   belongs_to :flat
+  mount_uploader :avatar_url, PhotoUploader
   has_many :assignments, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
