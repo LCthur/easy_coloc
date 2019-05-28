305d63c36b5c21417975fe85f6f19dfc90d88af1
Loic Thurre <loic.thurre@gmail.com>
2019-05-28 17:04:56 +0200

    issue new and create is working

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,17 +1,17 @@
 class IssuesController < ApplicationController
   def new
     @issue = Issue.new
-    @user = current_user
-    assignment = Assignment.find(params[:assignment_id])
-    @task = assignment.task
-    @user = assignment.user
+    @assignment = Assignment.find(params[:assignment_id])
+    @task = @assignment.task
+    @user = @assignment.user
   end
 
   def create
     @issue = Issue.new(issue_params)
+    @issue.assignment = Assignment.find(params[:assignment_id])
+    @issue.user = current_user
     if @issue.save
       redirect_to flat_path(current_user.flat)
-    else
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'assignments/:assignment_id/issues/new', to: "issues#new",     as: "new_issue"
-  post 'assignments/:assignment_id/issues',     to: "issues#create",  as: "create_issue"
   resources :flats, only: [:show, :index]
+  resources :assignments, only: [:create, :destroy] do
+    resources :issues, only: [:new, :create]
+  end
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
