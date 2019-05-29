b7a548625866a971180265867a8c5331dd43d2b2
Salcuni <giovanni.salcuni12@gmail.com>
2019-05-29 21:11:04 +0200

    indentation with sccs and view for must view

--- app/javascript/packs/components/checkForm.js
+++ app/javascript/packs/components/checkForm.js
@@ -1,19 +1,17 @@
 const inputs = document.querySelectorAll(".style-checkbox");
-const labels = document.querySelectorAll(".label-name");
-
 
 const inputState = () => {
     inputs.forEach((input) => {
-        input.addEventListener('click',(e) => {
+        input.addEventListener('click', (e) => {
             let click = e.currentTarget.getElementsByTagName("input")[0];
-                if(click.checked === false) {
-                    input.className = 'style-checkbox active';
-                    click.checked = true;
+            if (click.checked === false) {
+                input.className = 'style-checkbox active';
+                click.checked = true;
 
-                } else if (click.checked === true) {
-                    click.checked = false; 
-                    input.className = 'style-checkbox';
-                }
+            } else if (click.checked === true) {
+                click.checked = false;
+                input.className = 'style-checkbox';
+            }
         });
     });
 }
