5de687b94b0a4f1b279598e2787ea41787612b5b
Salcuni <giovanni.salcuni12@gmail.com>
2019-05-30 12:42:05 +0200

    drag and drog possible unless todo a done & listen a drop for send a data

--- app/javascript/packs/plugins/initSortable.js
+++ app/javascript/packs/plugins/initSortable.js
@@ -1,8 +1,32 @@
 import Sortable from 'sortablejs';
 
 const initSortable = () => {
-    const list = document.querySelector('.todo');
-    Sortable.create(list);
+    const endColumn = document.querySelector(".done-column")
+    const todo = document.querySelector(".todo")
+    const done = document.querySelector(".done")
+
+    new Sortable(todo, {
+        group: {
+            name: 'shared',
+            put: false // Do not allow items to be put into this list
+        },
+        ghostClass: "ghost",
+        animation: 150,
+        sort: false
+    });
+    
+    new Sortable(done, {
+        removeCloneOnHide: true,
+        ghostClass: "ghost",
+        group: 'shared',
+        animation: 150,
+        onChange: (event) => {
+            // alert(`${event.oldIndex} moved to ${event.newIndex}`);
+            endColumn.addEventListener('drop',(e) => {
+                console.log(e);
+            })
+          }
+    });
 };
   
 export { initSortable };
