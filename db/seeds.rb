282656d958a1fdec68d81fd058ce581df860302e
Salcuni <giovanni.salcuni12@gmail.com>
2019-05-31 15:38:14 +0200

    remove condition a true

--- db/seeds.rb
+++ db/seeds.rb
@@ -123,7 +123,7 @@ Assignment.create!(
   user_id: 1,
   task_id: 6,
   deadline: Date.today + rand(-3..5),
-  done_state: false
+  done_state: true
 )
 
 Assignment.create!(
@@ -131,7 +131,7 @@ Assignment.create!(
   user_id: 1,
   task_id: 7,
   deadline: Date.today + rand(-3..5),
-  done_state: false
+  done_state: true
 )
 
 p 'Crate issues'
