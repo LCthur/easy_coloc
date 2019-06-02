6cbec8af6cc8b5d2c3377e49300773da88e733a9
Loic Thurre <loic.thurre@gmail.com>
2019-06-02 20:33:04 +0200

    one more attempt with forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  skip_before_action :verify_authenticity_token
-  skip_before_filter :verify_authenticity_token
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
