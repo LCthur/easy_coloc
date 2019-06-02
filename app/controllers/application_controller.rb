6f25aba935d73282a2bb996dd223496f18d70964
Loic Thurre <loic.thurre@gmail.com>
2019-06-02 20:07:46 +0200

    attempt to fix deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token, raise: false
   before_action :authenticate_user!
-  skip_before_action :verify_authenticity_token  
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
