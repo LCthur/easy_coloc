0d726752cec8c7be2368bc6667de95f5bfee2956
Loic Thurre <loic.thurre@gmail.com>
2019-06-03 12:29:19 +0200

    modified navbar & redirection for the next user flow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar_photo])
   end
 
+  def after_sign_up_path_for(resource)
+    root_path
+  end
+
   def after_sign_in_path_for(resource)
     flat_path(current_user.flat)
   end
