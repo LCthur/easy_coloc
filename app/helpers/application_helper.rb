8b65e61735612e3c82c615a16b0fdd9cb63a3c4f
Loic Thurre <loic.thurre@gmail.com>
2019-06-03 14:17:54 +0200

    helper for days is done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,24 @@
 module ApplicationHelper
+  FRENCH_DAYS = {
+    "Today"     =>  "Aujourd'hui",
+    "Monday"    =>  "Lundi",
+    "Tuesday"   =>  "Mardi",
+    "Wednesday" =>  "Mercredi",
+    "Thursday"  =>  "Jeudi",
+    "Friday"    =>  "Vendredi",
+    "Saturday"  =>  "Samedi",
+    "Sunday"    =>  "Dimanche"
+  }
+
   def delayed?(assignment)
     assignment.deadline < Date.today
   end
 
   def full_name(first_name, last_name)
     "#{first_name.capitalize} #{last_name.capitalize}"
+  end
+
+  def day_french(day)
+    FRENCH_DAYS[day]
   end
 end
