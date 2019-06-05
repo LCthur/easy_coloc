2b564a84d14bd6d74319b806157a5ce6f80f5c5f
Salcuni <giovanni.salcuni12@gmail.com>
2019-06-05 11:38:19 +0200

    delete import

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@ import { initSortable } from './plugins/initSortable';
 import { initCustomInputFile } from "./components/customInputFile";
 import { initUpdateNavbarOnScroll } from './components/navbar';
 import { autoResize} from './components/textAreaAutoResize';
-import {popup} from './components/popup'
 
 if (document.getElementById('issue-page')) {
   initCustomInputFile();
