7e83a32e8cd96f75692272e72e4397bd7a825ed8
Loic Thurre <loic.thurre@gmail.com>
2019-06-06 13:32:16 +0200

    redirection

---
+++ app/javascript/packs/components/dealCounter.js
@@ -0,0 +1,21 @@
+
+// const checkBoxes = document.querySelectorAll('*[id^="id_"]');
+
+
+// const dealCounter = () => {
+//   checkBoxes.forEach((checkbox) => {
+//       let counter = 0
+//     checkbox.addEventListener('click', (e) => {
+//       if(counter === 0) {
+//         console.log(check);
+//         counter += 1
+//       } else {
+//         console.log(counter);
+//         counter -= 1
+//       }
+//     });
+//   });
+// };
+
+
+// export { dealCounter };
