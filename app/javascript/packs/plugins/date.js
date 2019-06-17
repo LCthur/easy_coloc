2be27ed54a85b32d4b47c8a5feee023bc5bc730b
Matthieu Borgognon <matrash@bqn.ch>
2019-06-17 17:01:18 +0200

    Added the add feature

---
+++ app/javascript/packs/plugins/date.js
@@ -0,0 +1,17 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/themes/airbnb.css'
+import { French } from "flatpickr/dist/l10n/fr.js"
+
+// const months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
+
+const flatpickrCreateTask = () => {
+  flatpickr(".datepicker", {
+    "locale": French,
+    minDate: new Date(),
+    altInput: true,
+    altFormat: "d M Y"
+  });
+}
+
+
+export { flatpickrCreateTask };
