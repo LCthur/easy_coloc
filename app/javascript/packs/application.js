003cf5ed6363efc0923d21d6fdb0a5b86dc7e250
Loic Thurre <loic.thurre@gmail.com>
2019-06-18 10:09:23 +0200

    replace of the flatpickr call to deploy

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,12 +7,12 @@ import { autoResize} from './components/textAreaAutoResize';
 import { flatpickrCreateTask } from "./plugins/date";
 // import { dealCounter} from './components/dealCounter';
 
-if (document.getElementById('issue-page')) {
-  initCustomInputFile();
-};
-
 if(document.getElementById('create-task')){
   flatpickrCreateTask();
+};
+
+if (document.getElementById('issue-page')) {
+  initCustomInputFile();
 };
 
 if(document.getElementById('colocation-title')){
